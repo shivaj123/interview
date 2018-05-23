8cdda5831ddb7b27c4d00102edc6a9f327ff0c73
shivaji <shivaji@hp>
2018-05-24 00:54:23 +0530

    added current date code

--- src/interview/date.java
+++ src/interview/date.java
@@ -8,12 +8,14 @@ public class date {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		DateFormat df= new SimpleDateFormat("yyyy/MM/dd HH:MM;SS");
-		Date d= new Date();
+	DateFormat df= new SimpleDateFormat("yyyy/MM/dd HH:MM;SS");
+	Date d= new Date();
 		String s= df.format(d);
 		System.out.println(s);
 		
 
 	}
 
-}
+	}
+
+
